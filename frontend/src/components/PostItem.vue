28c66cc2df3fb80f4fcc35e904f6a911b1806a42
Riyad <riyadtheone04@gmail.com>
2025-02-26 10:31:49 +0100

    initial commit

---
+++ frontend/src/components/PostItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="post">
+    <h3>{{ post.title }}</h3>
+    <p>{{ post.content }}</p>
+    <PostMenu @edit="$emit('edit', post)" @delete="$emit('delete', post.id)" />
+  </div>
+</template>
+
+<script>
+import PostMenu from "./PostMenu.vue";
+
+export default {
+  props: ["post"],
+  components: { PostMenu }
+};
+</script>
+
+<style scoped>
+.post { padding: 15px; border: 1px solid #ddd; margin-bottom: 10px; border-radius: 5px; }
+</style>
---
+++ frontend/src/components/PostMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="menu">
+    <button @click="toggleMenu" class="menu-button">⋮</button>
+    <ul v-if="menuOpen" class="menu-options">
+      <li @click="$emit('edit')">Edit</li>
+      <li @click="$emit('delete')">Delete</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { menuOpen: false };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.menu { position: relative; display: inline-block; }
+.menu-button { background: none; border: none; font-size: 18px; cursor: pointer; }
+.menu-options { position: absolute; right: 0; background: white; border: 1px solid #ddd; list-style: none; padding: 5px; }
+.menu-options li { padding: 5px; cursor: pointer; }
+.menu-options li:hover { background: #f0f0f0; }
+</style>
