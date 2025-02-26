63941d72d146bddd81a2e1c5b180e32731c2da39
ManarOuahabi <ouahabi.manar@etu.uae.ac.ma>
2025-02-26 10:37:00 +0000

    adding post and vote

---
+++ backend/models/votes.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Votes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Votes.init({
+    user_id: DataTypes.INTEGER,
+    post_id: DataTypes.INTEGER,
+    vote: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Votes',
+  });
+  return Votes;
+};
