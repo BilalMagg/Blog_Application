36cfed4e30abe5fdfe68bfd7f9fba7e4f905f429
khadija11-ws <khadijaouassi36@gmail.com>
2025-02-28 14:09:09 +0100

    modification dans usercontroller et userroutes

--- backend/routes/Userroutes.js
+++ backend/routes/Userroutes.js
@@ -2,12 +2,13 @@ const express=require('express');
 const router=express.Router();
 const authmiddleware=require('../middleware/authmiddleware');
 
-const {registerUser, loginUser, getUserProfile}=require('../controllers/usercontroller');
+const {registerUser, loginUser, getUserProfile, getallUserProfile}=require('../controllers/usercontroller');
  //route d'inscription
 router.post('/register',registerUser);
  //route de connexion
 router.post('/login',loginUser);
  //route de profile
-router.get('/me',authmiddleware,getUserProfile);
-
+ router.get('/profile',authmiddleware,getUserProfile);
+ router.get('/users',authmiddleware,getallUserProfile);
+ 
 module.exports=router;
