11530a9e8952b490f6c211877ef20db55eefccb6
ghizlane-aspic <ghizlanetouguii@gmail.com>
2025-03-01 15:18:28 +0000

    Actions utilisateur : Avatar ou Boutons Register/Login

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+   <NavBar /> 
+    <h1>Bienvenue sur le Blog</h1>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
