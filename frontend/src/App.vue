e6b5362deaa225db4b7dce8f9a83d9d1f9d89d3b
Riyad <riyadtheone04@gmail.com>
2025-03-01 21:01:47 +0100

    new style for create button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="nav-bar">
-      <router-link to="/create" class="nav-link">Create Post</router-link>
+      <router-link to="/create" class="nav-link">➕Create Post</router-link>
     </nav>
     <router-view />
   </div>
@@ -22,17 +22,21 @@ body {
   display: flex;
   justify-content: flex-start;
   padding: 15px 20px;
+  
 }
 
 .nav-link {
-  font-size: 18px;
+  font-size: 20px;
   font-weight: bold;
   color: green;
   text-decoration: none;
   padding: 10px 15px;
+  border-style: solid;
+  background-color:white;
+  border-color: white;
 }
 
 .nav-link:hover {
-  text-decoration: underline;
+  text-decoration:dotted;
 }
 </style>
