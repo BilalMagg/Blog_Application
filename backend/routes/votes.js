5b855db48bc361960d3c2472bcbb89eaec8b6c82
Bilal <gygcjg55@gmail.com>
2025-03-12 12:10:08 +0000

    Testing error middleware

--- backend/routes/votes.js
+++ backend/routes/votes.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const voteController = require('../controllers/voteController');
-const authMiddleware = require("../middleware/authMiddleware");
+// const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/", authMiddleware,voteController.votePost);
+router.post("/", voteController.votePost);
 // Ajouter un vote
 // router.post('/votes', voteController.addVote);
 
 // Récupérer tous les votes d'un post spécifique
-router.get('/posts/:post_id',authMiddleware, voteController.getVotesByPost);
+router.get('/posts/:post_id', voteController.getVotesByPost);
 
 // Récupérer tous les votes d'un utilisateur spécifique
-router.get("/users/:user_id",authMiddleware, voteController.getVotesByUser);
+router.get("/users/:user_id", voteController.getVotesByUser);
 
 // Mettre à jour un vote existant
-router.put("/:vote_id", authMiddleware, voteController.updateVote);
+router.put("/:vote_id",  voteController.updateVote);
 
 // Supprimer un vote
-router.delete("/:vote_id", authMiddleware, voteController.deleteVote);
+router.delete("/:vote_id",  voteController.deleteVote);
 
 
 module.exports = router;
